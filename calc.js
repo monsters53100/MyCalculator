2f24c7c8402e4ad5285779b07b219a44d031c14c
Nicola.Cortinovis <nicola.corti0@gmail.com>
2025-10-27 15:50:36 +0100

    conversioni dei valori da string a float, controllo se il numero è un number con stampa dell'errore, aggiunto uno switch con le varie operazioni, aggiunta del risultato a schermo, aggiunta documentazione

--- calc.js
+++ calc.js
@@ -1,13 +1,63 @@
-let result = 0;
+
 //arrey <HTMLButtonElement>
 let allBtn = document.querySelectorAll("button");
+let val1= document.getElementById("val1");
+let val2= document.getElementById("val2");
+let result = document.getElementById("result");
+
 allBtn.forEach(btn => {
     console.log(btn);
 
     btn.addEventListener("click", (event) => {
-        console.log(event.target);
+        console.log(event.target.attributes);
+        console.log(val1);
+
+
+        let a = parseFloat(val1.value);
+        let b = parseFloat(val2.value);
+
+        if (isNaN(a) || isNaN(b)) {
+            result.innerText = "Error: inserisci numeri validi";
+            return;
+        }
+
+        let operation = event.target.attributes["operation"].value;
+
+        let res = performOperation(a,b, operation);
+        result.innerText = "Result " + res;
+
+        if(res instanceof Error) {
+            result.innerText = "Error: " + res.message;
+        }else{
+            result.innerText = "Result " + res;
+        }
     })
 });
+
+/**
+ * Esegue l'operazione tra a e b
+ * @param a {number}
+ * @param b {number}
+ * @param operation {string} l'operazione da eseguire: + - * /
+ * @returns {*|Error}
+ */
+
+function performOperation(a, b, operation) {
+    switch (operation) {
+        case "+":
+            return somma(a,b);
+        case "-":
+            return sottrazione(a,b);
+        case "*":
+            return moltiplicazione(a,b);
+        case "/":
+            return divisione(a,b);
+        default:
+            return Error("Operazione non valida");
+
+    }
+
+}
 
 function somma(a,b){
     return a + b;
